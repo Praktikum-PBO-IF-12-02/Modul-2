cfc1de704ff2076fa7d1a07c2be45a9b9b168ebc
Muhammad Daffa Al Abiyyu <daffaklampok1@gmail.com>
2026-03-12 21:28:10 +0700

    Add files via upload

---
+++ 103112430039_Muhammad Daffa Al Abiyyu_Modul 2/Source/Fibonacci.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah deret: ");
+        int n = input.nextInt();
+
+        if (n <= 0) {
+            System.out.println("Input tidak valid. Jumlah harus lebih dari 0.");
+            return;
+        }
+
+        int a = 0, b = 1;
+        int total = 0, genap = 0, ganjil = 0;
+        int max = 0, min = 0;
+
+        System.out.println("\nDeret Fibonacci:");
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.print(a + " ");
+
+            total += a;
+
+            if (a % 2 == 0)
+                genap++;
+            else
+                ganjil++;
+
+            if (i == 0) {
+                min = a;
+                max = a;
+            } else {
+                if (a < min) min = a;
+                if (a > max) max = a;
+            }
+
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        double rata = (double) total / n;
+
+        System.out.println("\n\nTotal : " + total);
+        System.out.println("Rata-rata : " + rata);
+        System.out.println("Nilai terbesar : " + max);
+        System.out.println("Nilai terkecil : " + min);
+        System.out.println("Jumlah genap : " + genap);
+        System.out.println("Jumlah ganjil : " + ganjil);
+    }
+}
